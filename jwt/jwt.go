90af8904c96342b38ae5cd3ab2ce00b04570f359
Vladimir-Kuchinskyi <vladimir.kuchinskyi@koyfin.com>
2021-05-28 12:02:27 +0300

    feat: Add rotating keys

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,10 +1,13 @@
 package jwt
 
 import (
+	"crypto/rand"
 	"fmt"
+	"io"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	uuid "github.com/gofrs/uuid"
 )
 
 type UserClaims struct {
@@ -24,12 +27,73 @@ func (u *UserClaims) Valid() error {
 	return nil
 }
 
+type key struct {
+	key     []byte
+	created time.Time
+}
+
+var currentKID = ""
+var keys = map[string]key{}
+
+func GenerateNewKey() error {
+	newKey := make([]byte, 64)
+	_, err := io.ReadFull(rand.Reader, newKey)
+
+	if err != nil {
+		return fmt.Errorf("error in generateNewKey while generating a key: %w", err)
+	}
+
+	uid, err := uuid.NewV4()
+
+	if err != nil {
+		return fmt.Errorf("error in generateNewKey while generating UUID: %w", err)
+	}
+
+	keys[uid.String()] = key{
+		key:     newKey,
+		created: time.Now(),
+	}
+	currentKID = uid.String()
+
+	return nil
+}
+
 func CreateToken(u *UserClaims, key []byte) (string, error) {
 	t := jwt.NewWithClaims(jwt.SigningMethodHS512, u)
-	signedToken, err := t.SignedString(key)
+	signedToken, err := t.SignedString(currentKID)
 	if err != nil {
 		return "", fmt.Errorf("error in creatToken on token sign: %w", err)
 	}
 
 	return signedToken, nil
+}
+
+func ParseToken(token string, key []byte) (*UserClaims, error) {
+	t, err := jwt.ParseWithClaims(token, &UserClaims{}, func(t *jwt.Token) (interface{}, error) {
+		if t.Method.Alg() != jwt.SigningMethodHS512.Alg() {
+			return nil, fmt.Errorf("invalid signin algorithm")
+		}
+
+		kid, ok := t.Header["kid"].(string)
+		if !ok {
+			return nil, fmt.Errorf("invalid key ID")
+		}
+
+		k, ok := keys[kid]
+		if !ok {
+			return nil, fmt.Errorf("invalid key ID")
+		}
+
+		return k.key, nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error while verifying a token: %w", err)
+	}
+
+	if !t.Valid {
+		return nil, fmt.Errorf("jwt token in not valid")
+	}
+
+	return t.Claims.(*UserClaims), nil
 }
